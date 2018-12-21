1044bb791764c27530d892312b9d3abb7f348405
Vinesh Raju <vineshraju@rocketmail.com>
2018-12-21 20:55:11 +0530

    Changes Date format

--- app/src/main/java/app/vineshbuilds/news/home/viewmodel/ArticleVm.kt
+++ app/src/main/java/app/vineshbuilds/news/home/viewmodel/ArticleVm.kt
@@ -1,5 +1,6 @@
 package app.vineshbuilds.news.home.viewmodel
 
+import android.text.format.DateFormat
 import androidx.lifecycle.ViewModel
 import app.vineshbuilds.news.home.view.model.NewsModel.ArticleModel
 import java.text.SimpleDateFormat
@@ -7,7 +8,9 @@ import java.util.*
 
 class ArticleVm(val article: ArticleModel) : ViewModel() {
     val headline = article.headline
-    val publishedDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH).parse(article.publishedDate)
+    private val _publishedDate =
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH).parse(article.publishedDate)
+    val publishedDate = DateFormat.format("'AT 'hh:mm a' 'dd/MM/yyyy", _publishedDate)
     val urlToStory = article.urlToStory
     val thumbUrl = article.thumbUrl
     val story = article.story
